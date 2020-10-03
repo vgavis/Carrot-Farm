8debcd2b25585365d1e7c3f9e78768c7a1dc92ac
vgavis <vegardvaglid@hotmail.com>
2020-10-03 21:02:16 +0200

    Changes to language

--- js/levels.js
+++ js/levels.js
@@ -1,7 +1,7 @@
 var levels = [{
         name: 'grid-column-start 1',
         instructions: {
-            'en': '<p>Let us start to learn one of the first basic function in CSS: <code>grid-column-start</code> </p> As you can see the garden is divided into a grid map with squares where the carrots grow. To water the carrots you must point out where the carrots are. This can be done with the <code>grid-column-start</code> function. </p> Write <code>grid-column-start:2;</code> in the open field in the box below, to water the carrots in the 2nd column.</p>Hint: You can hover over the functions code in order to get a further explanation on what they do.</p>',
+            'en': '<p>Let us start to learn one of the basic functions in CSS: <code>grid-column-start</code> </p> As you can see, the garden is divided into a grid map with squares where the carrots grow. To water the carrots you must point out where the carrots are. This can be done with the <code>grid-column-start</code> function. </p> Write <code>grid-column-start:2;</code> in the open field in the box below to water the carrots in the 2nd column.</p>Hint: You can hover over the functions code in order to get a further explanation on what they do.</p>',
             'no': '<p>Velkommen til Gulerotgården.</p> I dette spillet vil du lære å skrive programmeringskode. Dette vil du lære gjennom å dyrke gulerøtter i en grønnsakshage. Hagen er delt inn i et rutenettkart med firkanter der gulrøttene vokser. Du skal vanne gulerøttene for å få dem til å vokse. For å vanne gulerøtter så flytter du vannet til området der gulroten vokser med<code>grid-column-start</code> funksjonen. </p> <p> Eksempel: om du skriver <code>grid-column-start: 2;</code> så vanner du gulerøttene i den andre kolonnen på rutenettet. </p> Tips: Du kan holde musepekeren over funksjonskoden for å få en ytterligere forklaring på hva de gjør. </p> ',
         },
         board: 'c',
@@ -13,7 +13,7 @@ var levels = [{
     {
         name: 'grid-column-start 2',
         instructions: {
-            'en': '<p>Well done! Let us try this again. Water the carrots where you see them in the garden. Remember to use the <code>grid-column-start</code> function that you used in the last level, but this time the carrots are on column 4 instead of 2. If you notice the box shaking it is because you made a mistake, but do not worry you can try again as many time as you want.</p>',
+            'en': '<p>Well done! Let us try this again. Water the carrots where you see them in the garden. Remember to use the <code>grid-column-start</code> function that you used in the last level, but this time the carrots are in column 4 instead of 2. If you notice the box shaking it is because you made a mistake, but do not worry, you can try again as many times as you want.</p>',
             'no': '<p>Dette var bra gjort! Nå skal vi prøve dette igjen. Vi skal vanne gulrøttene der du ser dem i hagen. Husk å bruke funksjonen <code>grid-column-start</code>.</p>',
         },
         board: 'c',
@@ -25,7 +25,7 @@ var levels = [{
     {
         name: 'grid-column-end 1',
         instructions: {
-            'en': '<p>We shall now learn the next step. The function <code>grid-column-start</code> alone only moves the water to the specified column stated, but with a <code>grid-column-end</code> function you can even water multiple carrots at once in several columns.</p><p>Try to use the <code>grid-column-end</code> function in order to water these 8 carrots. Make sure you do not water the dirt, as that would waste water. Keep in mind that the carrots starts at the 1st column and ends up until the 5th column.</p>',
+            'en': '<p>We shall now learn the next step. The function <code>grid-column-start</code> alone only waters the carrots in the specified column stated, but with a <code>grid-column-end</code> function you can also water carrots in several columns.</p><p>Try to use the <code>grid-column-end</code> function to water these 8 carrots. Make sure you do not water the dirt, because that would be a waste of water. Keep in mind that the carrots starts at the 1st column and ends up until the 5th column.</p>',
             'no': '<p>Nå skal vi lære noe nytt. Funksjonen <code>grid-column-start</code> flytter bare vannet til den kolonnen du gir nummer på. Med en <code>grid-column-end</code> -funksjon kan du også vanne flere gulrøtter samtidig i flere kolonner. </p> <p> Nå kan du forsøke å bruke funksjonen <code>grid-column-end</code> for å vanne disse 8 gulrøttene. Du må ikke vanne jorden, da dette er sløsing av vann. Husk at gulrøttene starter ved første kolonne og fram til 5. kolonne.</p>',
         },
         board: 'c',
@@ -38,7 +38,7 @@ var levels = [{
     {
         name: 'grid-column-end 2',
         instructions: {
-            'en': '<p>A tip: Remember that the functions <code>grid-column-start</code> and <code>grid-column-end</code>, can also work backwards, with the end value being smaller than the start value.</p><p>Try doing this by setting the value of the <code>grid-column-end</code> function lower than 5.</p>',
+            'en': '<p>A tip: Remember that the functions <code>grid-column-start</code> and <code>grid-column-end</code> can also work backwards, with the end value being smaller than the start value.</p><p>Try doing this by setting the value of the <code>grid-column-end</code> function lower than 5.</p>',
             'no': '<p>Et lite tips: funksjonene <code> grid-column-start </code> og <code> grid-column-end </code>, kan også fungere bakover, med da må sluttverdien være mindre enn startverdien. </p> <p> Nå kan du forsøke å gjøre dette ved å legg inn verdien til funksjonen <code> grid-column-end </code>lavere enn 5.</p>',
         },
         board: 'c',
@@ -64,7 +64,7 @@ var levels = [{
     {
         name: 'grid-column-start 3',
         instructions: {
-            'en': '<p>Negative values can also be used in the <code>grid-column-start</code> function. Try this by watering these carrots with a negative start value.</p>',
+            'en': '<p>Negative values can also be used in the <code>grid-column-start</code> function. Try this by watering these carrots using a negative start value.</p>',
             'no': '<p>Du kan også bruke negative verdier i funksjonen <code>grid-column-start</code>. Nå kan du prøve dette ved å vanne gulrøttene med en negativ startverdi.</p>',
         },
         board: 'c',
@@ -76,7 +76,7 @@ var levels = [{
     {
         name: 'grid-column-end 4',
         instructions: {
-            'en': '<p>Now it is time to learn a new keyword: You can define how many columns you wish to water with the <code>span</code> keyword. This means that when defining <code>grid-column-end</code> with <code>span 2</code> you define that the water will cover two columns wide.</p><p>For example, to water these carrots you can use the function: <code>grid-column-end: span 2;</code> How will you code to be able to water the carrots?</p>',
+            'en': '<p>Now it is time to learn a new keyword: You can define how many columns you wish to water with the <code>span</code> keyword. This means that combining <code>grid-column-end</code> with <code>span 2</code> will make the water cover two columns.</p><p>For example, to water these carrots you can use the function: <code>grid-column-end: span 3;</code> Try this for yourself now.</p>',
             'no': '<p>Nå skal du lære enda noe nytt. Du kan også definere hvor mange kolonner du vil vanne med <code> span </code> nøkkelordet. Dette betyr at når du skriver <code>grid-column-end</code> med <code>span 2</code>, så sier du at vannet skal dekke to kolonner bredt. Dette skrives som: <code> grid-column-end: span 2; </code> </p> <p> Hva vil du skrive for å kunne vanne gulrøttene du ser i hagen? </p>',
         },
         board: 'c',
@@ -115,7 +115,7 @@ var levels = [{
     {
         name: 'grid-column 1',
         instructions: {
-            'en': '<p>Now you will learn even more coding: Instead of having to type both <code>grid-column-start</code> and <code>grid-column-end</code> you can use the function <code>grid-column</code> to specify the start and end position at the same time. Make sure you seperate the start and end value with a slash.</p><p>This means that <code>grid-column: 2 / 4;</code> will water the area from column 2 and ending with column 4. See if you can water these carrots with this function.</p>',
+            'en': '<p>Now you will learn even more coding: Instead of having to type both <code>grid-column-start</code> and <code>grid-column-end</code> you can use the function <code>grid-column</code> to specify the start and end positions at the same time. Make sure you seperate the start and end value with a slash.</p><p>This means that <code>grid-column: 2 / 4;</code> will water the area from column 2 and ending with column 4. See if you can water these carrots using this function.</p>',
             'no': '<p>Nå skal du lære enda noe mer: I stedet for å skrive <code>grid-column-start</code> og <code>grid-column-end</code> kan du bruke funksjonen <code>grid-column</code> for å spesifisere starten og slutten. Vær sikker på at du skiller start- og sluttverdien med en skråstrek. </p> <p> Dette betyr at <code>grid-column: 2 / 4;</code> vil vanne området fra kolonne 2 og slutte med kolonne 4 . Nå kan du prøve vanne gulrøttene med denne funksjonen. </p>',
         },
         board: 'c',
@@ -151,7 +151,7 @@ var levels = [{
     {
         name: 'grid-row-start 2',
         instructions: {
-            'en': '<p>Tip: Just like with the column we can use <code>grid-row</code> in order to specify where the water will start and end. How would you use this function to water these carrots?</p>',
+            'en': '<p>Tip: Just like with the columns we can use <code>grid-row</code> in order to specify where the water will start and end. How would you use this function to water these carrots?</p>',
             'no': '<p>Tips: Akkurat som med kolonne kan vi bruke <code>grid-row</code> for å spesifisere hvor vannet skal starte og slutte på en rad. Nå kan du prøve å bruke denne funksjonen for å vanne gulrøttene? </p>',
         },
         board: 'c',
@@ -163,7 +163,7 @@ var levels = [{
     {
         name: 'grid-column-row 1',
         instructions: {
-            'en': '<p>Another tip: We can combine both these functions <code>grid-column</code> and <code>grid-row</code> , in order to move the water anywhere in the garden. How will you use these functions to water the carrots in the middle of the garden? Remember to use ; between each code line.</p>',
+            'en': '<p>Another tip: We can combine the functions <code>grid-column</code> and <code>grid-row</code> , in order to move the water anywhere in the garden. How will you use these functions to water the carrots in the middle of the garden? Remember to use ; between each line of code.</p>',
             'no': '<p>Nok et tips: Vi kan kombinere begge disse funksjonene <code>grid-column</code> og <code>grid-row</code>for å vanne hvor som helst i hagen. Hvordan vil du bruke disse funksjonene til å vanne gulrøttene midt i hagen? Husk bruk ; mellom ulike kodesetninger.</p>',
         },
         board: 'c',
@@ -187,7 +187,7 @@ var levels = [{
     {
         name: 'grid-area 1',
         instructions: {
-            'en': '<p>Now you will learn to combine code lines: An way to do this is by using the <code>grid-area</code> function.</p><p>This means that instead of writing <code>grid-column: 1/3 </code> and <code>grid-row: 1/6 </code> you can write <code>grid-area: 1 / 1 / 6 / 3;</code>. Try to use this function to water these carrots.</p>',
+            'en': '<p>Now you will learn to combine code lines: One way to do this is by using the <code>grid-area</code> function.</p><p>It will let us combine <code>grid-column: 1/3</code> and <code>grid-row: 1/6</code> into a single function:<code>grid-area: 1 / 1 / 6 / 3;</code>. Try to use this function to water these carrots.</p>',
             'no': '<p>Nå skal du lære å kombinere ulike kodesetninger: En enklere måte å gjøre det du utførte på nivå 15 er å bruke funksjonen <code>grid-area</code>. </p> <p> Dette betyr at i stedet for å skrive <code>grid-column: 1/3 </code> og <code>grid-row: 1/6 </code> kan du skrive <code>grid-area: 1 / 1 / 6 / 3;</code>. Nå kan du prøve dette til å vanne gulrøttene. </p> ',
         },
         board: 'c',
@@ -199,7 +199,7 @@ var levels = [{
     {
         name: 'grid-area 2',
         instructions: {
-            'en': '<p>Tip: If the area that needs watering does not form a perfect square, the area need to be divided into several squares. Theses squares one can put together so the whole area of carrots can be watered. The first square is already done for you, try using the <code>grid-area</code> function to water the rest of the carrots. Note that the areas can overlap eachother.</p>',
+            'en': '<p>Tip: If the area that needs watering does not form a perfect square, then you must divide it into several smaller squares. When these are combined, they will cover the whole area that needs water. The first square is already done for you, so try using the <code>grid-area</code> function to water the rest of the carrots. Note that the areas can overlap each other.</p>',
             'no': '<p>Tips: Hvis området som trenger vanning ikke er firkantet, så er det behov å dele området i ulike firkanter. Disse firkantene vil kunne settes sammen så hele området med gulerøtter vannes. Den ene firkanten er allerede programmert for deg; nå kan du prøve å bruke funksjonen <code>grid-area</code> or å vanne den andre firkanten, slik at alle gulrøttene blir vannet. Legg merke til at områdene kan overlappe hverandre. </p>',
         },
         board: 'cc',
@@ -213,7 +213,7 @@ var levels = [{
 var levelWin = {
     name: 'win',
     instructions: {
-        'en': '<p>Congratulations you have completed all the challenges!</p> Now you have learned the basics of using a CSS grid system. This is one the fundamental aspects of programming and a good start to get familiar with how programming works. With some further learning into this language you can learn how to design websites and create games. Creating this game used a lot of the language that you just learned.</p>You can go back to the levels that you completed by pressing where it says Level 17 of 17. You can also press Reset from this menu if you want to remove all your answers.</p>',
+        'en': '<p>Congratulations, you have completed all the challenges!</p> You have now learned the basics of using a CSS grid system. This is one of the fundamental aspects of programming and a good start to get familiar with how programming works. With some more learning and practice using this language, you can actually design websites and create games. Creating this game used a lot of the language that you have just learned.</p>You can go back to the levels that you completed by pressing where it says Level 17 of 17. You can also press Reset from this menu if you want to remove all your answers and begin again.</p>',
         'no': '<p>Gratulerer, du har fullført alle utfordringene! </p> Nå har du lært det grunnleggende i å bruke et CSS-rutenett-system. Dette er en av de viktigste delene ved programmering og er en god start for å bli kjent med hvordan programmering fungerer. Med litt videre læring av dette språket kan du lære å designe nettsteder og å lage spill. Å lage dette spillet brukte mye av språket du nettopp har lært. </p> Du kan gå tilbake til nivåene du fullførte ved å trykke der det står Nivå 17 av 17. Du kan også trykke på Tilbakestill fra denne menyen hvis du vil fjerne alle svarene dine.</p>',
     },
     board: '',
